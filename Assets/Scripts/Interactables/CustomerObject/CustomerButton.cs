78663207206ebc4306bba238d63bb062de566dab
Lassi Niinikorpi <lassi.n94@gmail.com>
2020-04-18 14:18:08 +0300

    add customer

---
+++ Assets/Scripts/Interactables/CustomerObject/CustomerButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerButton : MonoBehaviour
+{
+    [SerializeField]
+    Customer customer;
+    public void PressButton() {
+        SellTicket();
+    }
+
+    void SellTicket()
+    {
+        customer.BuyTicket();
+    }
+}
