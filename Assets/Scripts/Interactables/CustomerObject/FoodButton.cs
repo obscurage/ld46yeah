c7dd089d6590e74a2ce0da35efb3cbf28400432b
Lassi Niinikorpi <lassi.n94@gmail.com>
2020-04-18 20:26:54 +0300

    Continuing cart

---
+++ Assets/Scripts/Interactables/CustomerObject/FoodButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodButton : MonoBehaviour
+{
+    [SerializeField]
+    Customer customer;
+    public void PressButton()
+    {
+        SellFood();
+    }
+
+    void SellFood()
+    {
+        customer.BuyFood();
+    }
+}
