65830436973f5c1389b6b82bbd74208d4a553a27
Lassi Niinikorpi <lassi.n94@gmail.com>
2020-04-18 11:54:52 +0300

    Import textmesh pro

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+    }
+
+    private void CalculateMovement()
+    {
+        Vector2 movement = new Vector2(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0);
+        transform.Translate(movement);
+    }
+}
