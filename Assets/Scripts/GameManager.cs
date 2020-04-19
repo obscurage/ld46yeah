5ce2da3bdea3b01c042cfe6c5acdc05ae81bc697
Lassi Niinikorpi <lassi.n94@gmail.com>
2020-04-19 16:23:48 +0300

    add smoke and rails and kärry

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -79,6 +79,7 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
+        startMenu.SetActive(true);
         backgroundMusicPlayer.requestClipChange(1);        
 
         maleVoice = Resources.LoadAll<AudioClip>("CharactersVoices/Male");
